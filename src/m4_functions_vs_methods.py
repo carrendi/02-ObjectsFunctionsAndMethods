9af4f59a10e568c34bf28e31e95faaad2dbc33c8
Carrendi <carrendi@rose-hulman.edu>
2019-01-29 00:07:10 -0500

    m1

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -4,11 +4,11 @@ Demonstrates using (calling) FUNCTIONS and using (calling) METHODS:
   -- how they differ.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Danna Carreno.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   With your instructor, READ the file   methods_vs_functions.txt
 #   in this project, ASKING QUESTIONS as needed to understand its contents.
 #   After you have done so, mark this _TODO_ as DONE
@@ -98,7 +98,7 @@ def draw_many_squares(my_turtle, number_of_squares, size, twist):
         my_turtle.left(twist)
 
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   There are four FUNCTIONS defined ABOVE this:
 #     main
 #     run_example
@@ -126,6 +126,13 @@ def draw_many_squares(my_turtle, number_of_squares, size, twist):
 
 
 def try_methods():
+    rori = rg.SimpleTurtle()
+    rori.pen = rg.Pen('brown',5)
+    rori.forward(150)
+    rori.left(90)
+    rori.forward(50)
+    rori.backward(100)
+
     """
     Constructs a SimpleTurtle and sets its   pen   to a new rg.Pen
     that is 'brown' with thickness 5.
@@ -136,7 +143,7 @@ def try_methods():
       -- backward  100 units
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function, per its doc-string above.
+    # DONE: 4. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     ###########################################################################
 
@@ -145,6 +152,25 @@ def try_methods():
 # IMPORTANT: Read the NOTE below before you try to implement the next function!
 ###############################################################################
 def try_functions():
+    kim = rg.SimpleTurtle()
+    kim.pen_up()
+    kim.go_to(rg.Point(200,100))
+    kim.pen_down()
+    kim.pen = rg.Pen('brown',5)
+    kim.go_to(rg.Point(300,30))
+    kourt = rg.SimpleTurtle()
+    kourt.pen_up()
+    kourt.go_to(rg.Point(100,200))
+    kourt.pen = rg.Pen('purple', 2)
+    kourt.pen_down()
+    kourt.go_to(rg.Point(0,0))
+    khloe = rg.SimpleTurtle()
+    khloe.pen_up()
+    khloe.go_to(rg.Point(-50,50))
+    khloe.pen_down()
+    khloe.pen = rg.Pen('pink', 4)
+    khloe.go_to(rg.Point(100,100))
+
     """
     Causes several SimpleTurtles to do the following:
      -- One jumps to (200, 100), then moves (while drawing) to (300, 30)
@@ -152,7 +178,7 @@ def try_functions():
      -- One jumps to (-50, 50), then moves (while drawing) to (100, 100)
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function, per its doc-string above.
+    # DONE: 5. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #    NOTE: This function requires
@@ -166,6 +192,60 @@ def try_functions():
 # IMPORTANT: Read the NOTE below before you try to implement the next function!
 ###############################################################################
 def try_methods_and_functions():
+    rob = rg.SimpleTurtle('square')
+    rob.pen = rg.Pen('blue', 5)
+    rob.backward(150)
+    rob.speed = 1
+
+    rob.forward(100)
+    rob.left(90)
+    rob.forward(100)
+    rob.left(90)
+    rob.forward(100)
+    rob.left(90)
+    rob.forward(100)
+
+    rob.left(30)
+
+    rob.forward(100)
+    rob.left(90)
+    rob.forward(100)
+    rob.left(90)
+    rob.forward(100)
+    rob.left(90)
+    rob.forward(100)
+
+    rob.speed = 5
+    rob.pen = rg.Pen('red', 5)
+    for _ in range(10):
+        rob.forward(50)
+        rob.left(90)
+        rob.forward(50)
+        rob.left(90)
+        rob.forward(50)
+        rob.left(90)
+        rob.forward(50)
+
+        rob.left(15)
+    rob.speed = 100
+    rob.pen = rg.Pen('red', 35)
+    for _ in range(8):
+        rob.forward(300)
+        rob.left(90)
+        rob.forward(300)
+        rob.left(90)
+        rob.forward(300)
+        rob.left(90)
+        rob.forward(300)
+
+        rob.left(60)
+    rob.pen = rg.Pen('black', 3)
+    rob.backward(200)
+    rob.draw_circle(30)
+    rob.draw_square(50)
+
+
+
     """
     Constructs a SimpleTurtle and sets its   pen  to a new rg.Pen
     that is 'blue' with thickness 5.
@@ -198,7 +278,7 @@ def try_methods_and_functions():
       8. Draw a SQUARE whose sides are each of length 50.
     """
     ###########################################################################
-    # TODO: 6. Implement and test this function, per its doc-string above.
+    # DONE: 6. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #   NOTE: This function should ** CALL ** the
